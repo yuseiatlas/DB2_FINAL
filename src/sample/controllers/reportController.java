ba1516689b50cfe2a11d659aa91657f5a90ef605
Jawad Mashhadieh <rasengan40@gmail.com>
2017-01-16 16:52:01 +0200

    report done

--- src/sample/controllers/reportController.java
+++ src/sample/controllers/reportController.java
@@ -11,6 +11,7 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import sample.models.report;
 
 import java.sql.*;
+import java.util.Calendar;
 
 public class reportController{
 
@@ -34,26 +35,55 @@ public class reportController{
     }
 
     public void buildTableData() throws SQLException {
-        Statement stmt = vDatabaseConnection.createStatement();
-        String query = "Select * from bonus";
-        ResultSet rs = stmt.executeQuery(query);
-        reportData.clear();
-        while (rs.next()) {
-            int employeeId = rs.getInt("employeeid");
-            ResultSet employeeRS=vDatabaseConnection.createStatement()
-                    .executeQuery("select * from employee where id="+employeeId);
-            employeeRS.next();
-            String salary = "";
-            reportData.add(new report(employeeId,employeeRS.getString("FIRSTNAME")+' '+employeeRS.getString("LASTNAME"),salary));
-        }
+//        Statement stmt = vDatabaseConnection.createStatement();
+//        String query = "Select * from bonus";
+//        ResultSet rs = stmt.executeQuery(query);
+//        reportData.clear();
+//        while (rs.next()) {
+//            int employeeId = rs.getInt("employeeid");
+//            ResultSet employeeRS=vDatabaseConnection.createStatement()
+//                    .executeQuery("select * from employee where id="+employeeId);
+//            employeeRS.next();
+//            String salary = "";
+//            reportData.add(new report(employeeId,employeeRS.getString("FIRSTNAME")+' '+employeeRS.getString("LASTNAME"),salary));
+//        }
         reportTable.setItems(reportData);
     }
     public void handleSearch(ActionEvent actionEvent) throws SQLException {
-        String query="SALARYSUM(11)";
+        String query="{?=call GETSALARY(?,?)}";
         CallableStatement statement=vDatabaseConnection.prepareCall(query);
-        statement.registerOutParameter(1, Types.INTEGER);
-        statement.execute();
-        System.out.println(statement.getInt(1));
+        ResultSet rs=vDatabaseConnection.createStatement().executeQuery("select * from employee");
+        if(searchField.getValue()==null){
+            System.out.println("search field is null");
+            return;
+        }
+            reportData.clear();
+        statement.setInt(2,searchField.getValue().getMonthValue());
+        while(rs.next()){
+            statement.setInt(3,rs.getInt("id"));
+            statement.registerOutParameter(1, Types.INTEGER);
+            statement.execute();
+            reportData.add(new report(rs.getString("FIRSTNAME")+' '+rs.getString("LASTNAME"),Integer.toString(statement.getInt(1))));
+        }
+       // System.out.println(reportData.get(1));
         //buildTableData();
+        reportTable.setItems(reportData);
+
+
+        //System.out.println(statement.getInt(1));
+
+//        ResultSet rs=vDatabaseConnection.createStatement().executeQuery("select * from employee");
+//        while(rs.next()){
+//            statement=vDatabaseConnection.prepareCall(query);
+//            statement.registerOutParameter(1, Types.INTEGER);
+//            statement.setInt(2,11);
+//            statement.setInt(3, rs.getInt("id"));
+//            String salary = Integer.toString(statement.getInt(1));
+//            String fullName=rs.getString("FIRSTNAME")+' '+rs.getString("LASTNAME");
+//            reportData.add(new report(rs.getInt("id"),fullName, salary));
+//        }
+//        statement.execute();
+//        //System.out.println(statement.getInt(1));
+//        buildTableData();
     }
 }
--- src/sample/models/report.java
+++ src/sample/models/report.java
@@ -3,13 +3,37 @@ package sample.models;
 import javafx.beans.property.SimpleStringProperty;
 
 public class report{
-    SimpleStringProperty id;
+   // SimpleStringProperty id;
     SimpleStringProperty employee;
     SimpleStringProperty salary;
 
-    public report(int id,String employee, String salary) {
-        this.id=new SimpleStringProperty(Integer.toString(id));
+    public report(String employee, String salary) {
+        //this.id=new SimpleStringProperty(Integer.toString(id));
         this.employee = new SimpleStringProperty(employee);
         this.salary = new SimpleStringProperty(salary);
+    }
+
+    public String getEmployee() {
+        return employee.get();
+    }
+
+    public SimpleStringProperty employeeProperty() {
+        return employee;
+    }
+
+    public void setEmployee(String employee) {
+        this.employee.set(employee);
+    }
+
+    public String getSalary() {
+        return salary.get();
+    }
+
+    public SimpleStringProperty salaryProperty() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary.set(salary);
     }
 }
